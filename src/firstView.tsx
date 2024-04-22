d6e42bbfcd82469b5301185b139a8b1dd17c7cf2
kinako1415 <Owata09@outlook.jp>
2024-04-22 14:46:46 +0900

    svgのインライン化

--- src/firstView.tsx
+++ src/firstView.tsx
@@ -1,22 +1,23 @@
-import './firstView.css'
-import Mysite from './assets/MysiteSvg/Mysite.svg';
-import cloud from './assets/MysiteSvg/cloud.svg';
-import star1 from './assets/MysiteSvg/star1.svg';
-import star2 from './assets/MysiteSvg/star2.svg';
-import star3 from './assets/MysiteSvg/star3.svg';
-import barcode from './assets/MysiteSvg/barcode.svg';
-import Mysitebg from './assets/MysiteSvg/Mysitebg.svg';
+import "./firstView.css";
+import Mysite from "./assets/MysiteSvg/Mysite.svg";
+import cloud from "./assets/MysiteSvg/cloud.svg";
+import star1 from "./assets/MysiteSvg/star1.svg";
+import star2 from "./assets/MysiteSvg/star2.svg";
+import star3 from "./assets/MysiteSvg/star3.svg";
+import barcode from "./assets/MysiteSvg/barcode.svg";
+import Mysitebg from "./assets/MysiteSvg/Mysitebg.svg";
+import { ReactSVG } from "react-svg";
 
 export default function FirstView() {
-    return (
-        <>
-            <img src={star1} />
-            <img src={star2} />
-            <img src={star3} />
-            <img src={cloud} />
-            <img src={Mysite} />
-            <img src={barcode} />
-            <img src={Mysitebg} />
-        </>
-    )
+  return (
+    <>
+      <ReactSVG src={star1} className="outlineSvg"></ReactSVG>
+      <ReactSVG src={star2} className="outlineSvg"></ReactSVG>
+      <ReactSVG src={star3} className="outlineSvg"></ReactSVG>
+      <ReactSVG src={Mysite}></ReactSVG>
+      <ReactSVG src={barcode} className="outlineSvg"></ReactSVG>
+      <ReactSVG src={Mysitebg} className="outlineSvg"></ReactSVG>
+      <ReactSVG src={cloud} className="outlineSvg"></ReactSVG>
+    </>
+  );
 }
