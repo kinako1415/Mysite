d07fafe8625d9c153d9105eb0d972d018468777d
kinako1415 <Owata09@outlook.jp>
2024-04-22 11:25:20 +0900

    add bercode

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import cloud from './assets/MysiteSvg/cloud.svg';
 import star1 from './assets/MysiteSvg/star1.svg';
 import star2 from './assets/MysiteSvg/star2.svg';
 import star3 from './assets/MysiteSvg/star3.svg';
+import bercode from './assets/MysiteSvg/bercode.svg';
 import './App.css'
 
 
@@ -11,11 +12,12 @@ function App() {
 
   return (
     <>
-      <img src={star1} className='drugCancel' />
+      <img src={star1} />
       <img src={star2} />
       <img src={star3} />
       <img src={cloud} />
-      <img src={Mysite} className='drugCancel' />
+      <img src={Mysite} />
+      <img src={bercode} />
     </>
   )
 }
