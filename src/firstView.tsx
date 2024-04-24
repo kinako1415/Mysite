b058c5af50ecab19326881da331b3e32afa56588
kinako1415 <Owata09@outlook.jp>
2024-04-24 11:27:40 +0900

    styled-componentの追加

--- src/firstView.tsx
+++ src/firstView.tsx
@@ -6,18 +6,29 @@ import star2 from "./assets/MysiteSvg/star2.svg";
 import star3 from "./assets/MysiteSvg/star3.svg";
 import barcode from "./assets/MysiteSvg/barcode.svg";
 import Mysitebg from "./assets/MysiteSvg/Mysitebg.svg";
+import MysiteEffect from "./assets/MysiteSvg/MysiteEffect.svg";
 import { ReactSVG } from "react-svg";
 
 export default function FirstView() {
   return (
     <>
-      <ReactSVG src={star1} className="outlineSvg"></ReactSVG>
-      <ReactSVG src={star2} className="outlineSvg"></ReactSVG>
-      <ReactSVG src={star3} className="outlineSvg"></ReactSVG>
-      <ReactSVG src={Mysite}></ReactSVG>
-      <ReactSVG src={barcode} className="outlineSvg"></ReactSVG>
-      <ReactSVG src={Mysitebg} className="outlineSvg"></ReactSVG>
-      <ReactSVG src={cloud} className="outlineSvg"></ReactSVG>
+      <div className="topbg">
+        <ReactSVG src={star2} className="outlineSvg star2-1"></ReactSVG>
+        <ReactSVG src={star2} className="outlineSvg star2-2"></ReactSVG>
+        <ReactSVG src={star2} className="outlineSvg star2-3"></ReactSVG>
+        <ReactSVG src={star3} className="outlineSvg star3-1"></ReactSVG>
+        <ReactSVG src={barcode} className="outlineSvg bercode"></ReactSVG>
+        <ReactSVG src={cloud} className="outlineSvg cloud1-1"></ReactSVG>
+        <ReactSVG src={cloud} className="outlineSvg cloud1-2"></ReactSVG>
+        <ReactSVG src={cloud} className="outlineSvg cloud1-3"></ReactSVG>
+      </div>
+      <div className="topLogo">
+        <ReactSVG src={star1} className="outlineSvg star1-1"></ReactSVG>
+        <ReactSVG src={star1} className="outlineSvg star1-2"></ReactSVG>
+        <ReactSVG src={Mysite} className="MysiteLogo"></ReactSVG>
+        <ReactSVG src={MysiteEffect} className="MysiteEffect"></ReactSVG>
+        <ReactSVG src={Mysitebg} className="Mysitebg"></ReactSVG>
+      </div>
     </>
   );
 }
