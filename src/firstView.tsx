202e752c226afca6f0d0f84476ca516256c59a0a
kinako1415 <Owata09@outlook.jp>
2024-04-25 10:58:47 +0900

    styled-conponentの追加

--- src/firstView.tsx
+++ src/firstView.tsx
@@ -1,4 +1,5 @@
 import "./firstView.css";
+import styled from "styled-components";
 import Mysite from "./assets/MysiteSvg/Mysite.svg";
 import cloud from "./assets/MysiteSvg/cloud.svg";
 import star1 from "./assets/MysiteSvg/star1.svg";
@@ -9,26 +10,44 @@ import Mysitebg from "./assets/MysiteSvg/Mysitebg.svg";
 import MysiteEffect from "./assets/MysiteSvg/MysiteEffect.svg";
 import { ReactSVG } from "react-svg";
 
+const Topbg = styled.div`
+  position: relative;
+  z-index: 0;
+  stroke: #000000;
+  stroke-width: 1px;
+`;
+
+const TopLogo = styled.div`
+  position: relative;
+  transform: scale(0.75);
+  left: 3.4rem;
+  top: 7rem;
+  z-index: 10;
+`;
+
 export default function FirstView() {
   return (
     <>
-      <div className="topbg">
-        <ReactSVG src={star2} className="outlineSvg star2-1"></ReactSVG>
-        <ReactSVG src={star2} className="outlineSvg star2-2"></ReactSVG>
-        <ReactSVG src={star2} className="outlineSvg star2-3"></ReactSVG>
-        <ReactSVG src={star3} className="outlineSvg star3-1"></ReactSVG>
-        <ReactSVG src={barcode} className="outlineSvg bercode"></ReactSVG>
-        <ReactSVG src={cloud} className="outlineSvg cloud1-1"></ReactSVG>
-        <ReactSVG src={cloud} className="outlineSvg cloud1-2"></ReactSVG>
-        <ReactSVG src={cloud} className="outlineSvg cloud1-3"></ReactSVG>
-      </div>
-      <div className="topLogo">
+      <Topbg>
+        <ReactSVG src={star2} className="star2-1"></ReactSVG>
+        <ReactSVG src={star2} className="star2-2"></ReactSVG>
+        <ReactSVG src={star2} className="star2-3"></ReactSVG>
+        <ReactSVG src={star3} className="star3-1"></ReactSVG>
+        <ReactSVG src={star3} className="star3-2"></ReactSVG>
+        <ReactSVG src={barcode} className="bercode"></ReactSVG>
+        <ReactSVG src={cloud} className="cloud1-1"></ReactSVG>
+        <ReactSVG src={cloud} className="cloud1-2"></ReactSVG>
+        <ReactSVG src={cloud} className="cloud1-3"></ReactSVG>
+        <ReactSVG src={cloud} className="cloud1-4"></ReactSVG>
+        <ReactSVG src={cloud} className="cloud1-5"></ReactSVG>
+      </Topbg>
+      <TopLogo>
         <ReactSVG src={star1} className="outlineSvg star1-1"></ReactSVG>
         <ReactSVG src={star1} className="outlineSvg star1-2"></ReactSVG>
         <ReactSVG src={Mysite} className="MysiteLogo"></ReactSVG>
         <ReactSVG src={MysiteEffect} className="MysiteEffect"></ReactSVG>
         <ReactSVG src={Mysitebg} className="Mysitebg"></ReactSVG>
-      </div>
+      </TopLogo>
     </>
   );
 }
